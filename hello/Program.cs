df853aa12bb577f1351a06770e753af5cec79d36
Matheus Castello <matheus@castello.eng.br>
2020-06-12 14:13:13 -0300

    dotnet: hello: Simple Hello World
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ hello/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Arch {RuntimeInformation.OSArchitecture}");
+            Console.WriteLine($"OS {RuntimeInformation.OSDescription}");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
